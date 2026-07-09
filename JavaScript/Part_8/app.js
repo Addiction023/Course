e75773e67eb268730f249be6c4640782e92a158b
Addiction023 <atulpatel09876@gmail.com>
2026-07-09 18:15:06 +0530

    Updating...

--- JavaScript/Part_8/app.js
+++ JavaScript/Part_8/app.js
@@ -1,29 +1,29 @@
-// let arr = [1, 2, 3, 4, 5];
-// let sum = arr.reduce((min, el) => {
-//     if (el < min) {
-//         return el;
-//     } else {
-//         return min;
-//     }
-// }, arr[0]);
-// console.log(sum); // Output: 1
+let arr = [1, 2, 3, 4, 5];
+let sum = arr.reduce((min, el) => {
+    if (el < min) {
+        return el;
+    } else {
+        return min;
+    }
+}, arr[0]);
+console.log(sum); // Output: 1
 
 
-// const avg = (...arr) => {
-//     let sum = arr.map((ele) => ele * ele).reduce((res, ele) => (res + ele));
-//     return sum / arr.length;
-// };
+const avg = (...arr) => {
+    let sum = arr.map((ele) => ele * ele).reduce((res, ele) => (res + ele));
+    return sum / arr.length;
+};
 
-// console.log(avg(1, 2, 3, 4, 5));
-
-
-// const avg = (...arr) => {
-//     return sum = arr.map((ele) => ele + 5);
-// };
-
-// console.log(avg(1, 2, 3, 4, 5));
+console.log(avg(1, 2, 3, 4, 5));
 
 
-// const UperCase = (...arr) => arr.map((ele) => ele.toUpperCase());
+const avg = (...arr) => {
+    return sum = arr.map((ele) => ele + 5);
+};
 
-// console.log(UperCase("atul", "durga", "chandra")); 
+console.log(avg(1, 2, 3, 4, 5));
+
+
+const UperCase = (...arr) => arr.map((ele) => ele.toUpperCase());
+
+console.log(UperCase("atul", "durga", "chandra")); 
