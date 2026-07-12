d8782a30406da3d084d1bc8538f300cebbe78410
Addiction023 <atulpatel09876@gmail.com>
2026-07-12 16:53:29 +0530

    update

--- JavaScript/Part_10/app.js
+++ JavaScript/Part_10/app.js
@@ -1,25 +1,25 @@
 let btn = document.querySelector("button");
 let h1 = document.querySelector("h1");
 let div = document.querySelector("div");
-// btn.addEventListener("click", function () {
-//     let id = setInterval(() => {
-//         let R = Math.floor(Math.random() * 255);
-//         let G = Math.floor(Math.random() * 255);
-//         let B = Math.floor(Math.random() * 255);
-//         let color = (`rgb(${R}, ${G}, ${B})`)
-//         h1.innerHTML = color;
-//         div.style.backgroundColor = color;
-//         console.log("hi");
+btn.addEventListener("click", function () {
+    let id = setInterval(() => {
+        let R = Math.floor(Math.random() * 255);
+        let G = Math.floor(Math.random() * 255);
+        let B = Math.floor(Math.random() * 255);
+        let color = (`rgb(${R}, ${G}, ${B})`)
+        h1.innerHTML = color;
+        div.style.backgroundColor = color;
+        console.log("hi");
 
-//     }, 1000);
+    }, 1000);
 
-//     setTimeout(() => {
-//         clearInterval(id);
-//         h1.innerHTML = "Generate a random color";
-//         div.style.backgroundColor = (`rgb(255,255,255)`);
-//         console.log("bye");
-//     }, 5000);
-// });
+    setTimeout(() => {
+        clearInterval(id);
+        h1.innerHTML = "Generate a random color";
+        div.style.backgroundColor = (`rgb(255,255,255)`);
+        console.log("bye");
+    }, 5000);
+});
 
 let image = document.querySelector("img");
 
